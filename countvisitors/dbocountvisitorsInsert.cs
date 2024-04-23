8f0d8b126c91e5f65226ff3ea568c057c68be7f3
Mihai Marinescu <mihai.marinescu@inchcapeservices.com>
2024-04-23 23:11:19 +0300

    Created function to return visitors

---
+++ countvisitors/dbocountvisitorsInsert.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.Sql;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class dbocountvisitorsInsert
+    {
+        // Visit https://aka.ms/sqlbindingsoutput to learn how to use this output binding
+        [FunctionName("dbocountvisitorsInsert")]
+        public static async Task<CreatedResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Sql("[dbo].[count_visitors]", "SqlConnectionString")] IAsyncCollector<ToDoItem> output,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger with SQL Output Binding function processed a request.");
+
+            // Generate a random visitor count
+            Random random = new Random();
+            int visitorCount = random.Next();
+
+            // Create a new ToDoItem with auto-generated ID and visitor count
+            ToDoItem todoItem = new ToDoItem
+            {
+                visitor_name = visitorCount // Random visitor count
+            };
+
+            // Add the ToDoItem to the SQL database
+            await output.AddAsync(todoItem);
+
+            // Return a CreatedResult with the inserted item
+            return new CreatedResult(req.Path, todoItem);
+        }
+    }
+
+    public class ToDoItem
+    {
+        public string id { get; set; } // Auto-generated ID
+        public int visitor_name { get; set; } // Random visitor count
+    }
+}
